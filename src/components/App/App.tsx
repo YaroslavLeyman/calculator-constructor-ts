5d264c51040f8714a93024edfa1f1559d800e8c1
Iaroslav Leiman <yaroslavleiman@yandex.ru>
2023-05-15 23:04:33 +0700

    deployed on gh-pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,24 @@
-import {Canvas} from "../Canvas/Canvas";
-import {Sidebar} from "../Sidebar/SideBar";
-import {SwitcherView} from "../Switcher/SwitcherView/SwitcherView";
-import {useAppSelector} from "../../hooks/useAppSelector";
+import { Canvas } from "../Canvas/Canvas";
+import { Sidebar } from "../Sidebar/SideBar";
+import { SwitcherView } from "../Switcher/SwitcherView/SwitcherView";
+import { useAppSelector } from "../../hooks/useAppSelector";
 
-import styles from './App.module.css'
-
+import styles from "./App.module.css";
 
 function App() {
-
-  const mode = useAppSelector(state => state.app.mode)
+  const mode = useAppSelector((state) => state.app.mode);
 
   return (
     <div className={styles.app}>
       <div className={styles.mainContainer}>
-        <SwitcherView/>
+        <SwitcherView />
         <div className={styles.container}>
-          {mode === 'constructor' && <Sidebar/>}
-          <Canvas/>
+          {mode === "constructor" && <Sidebar />}
+          <Canvas />
         </div>
       </div>
     </div>
-  )
-
+  );
 }
 
 export default App;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,17 @@
-import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit';
-import {appReducer} from "../constants/appFragment";
-
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { appReducer } from "../constants/appFragment";
 
 export const store = configureStore({
-    reducer: {
-        app: appReducer,
-    }
+  reducer: {
+    app: appReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    Action<string>
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
 >;
